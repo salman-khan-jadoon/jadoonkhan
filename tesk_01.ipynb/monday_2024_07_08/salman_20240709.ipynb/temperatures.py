a79a88f43b4aee7237d0e05d95af4d732aa246bf
salman-khan270 <wwwmsalman98@gmail.com>
2024-08-12 00:53:16 -0700

    s jadoon
    s jadoon

---
+++ tesk_01.ipynb/monday_2024_07_08/salman_20240709.ipynb/temperatures.py
@@ -0,0 +1,25 @@
+# temperatures = int(input("please enter temperatures"))
+# if temperatures <=0:
+#     print("it is freezing", temperatures) 
+# elif temperatures >=0 and temperatures <=15:
+#     print("it is cold ",temperatures)
+# elif temperatures >=15 and temperatures <=25:
+#     print("it is warm ",temperatures)
+# else: 
+#     print("it is invalieled", temperatures)
+
+
+# temperature = int(input("please enter temperature:"))
+# if temperature <=0:
+#     print("it is freezing",temperature )
+# elif temperature >=0 and temperature<= 15:
+#     print("it is cold",temperature)
+
+
+temperature = int(input("please enter the temperature"))
+if temperature <=0:
+    print("it is freezing ", temperature)
+elif temperature >=0 and temperature <=15:
+    print("it is cold " , temperature)
+elif temperature>=15 and temperature<=25:
+    print("it is warm", temperature)
